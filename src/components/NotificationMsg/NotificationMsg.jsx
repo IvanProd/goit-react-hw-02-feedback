3532d08a53aa68dbd0a793af9ea9567613408b0f
Ivan Prodeus <learning.webpractice@gmail.com>
2022-10-19 21:12:07 +0300

    maked notification

---
+++ src/components/NotificationMsg/NotificationMsg.jsx
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import { NotifyTitle } from './NotitficationMsg.styled';
+
+export const NotificationMsg = () => {
+    return <NotifyTitle>No feedback given</NotifyTitle>;
+};
