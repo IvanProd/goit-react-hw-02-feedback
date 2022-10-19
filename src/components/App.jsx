3295dbafe1ff6cae6dde1147ce97d6a60a65f2d2
Ivan Prodeus <learning.webpractice@gmail.com>
2022-10-19 21:45:53 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,35 +3,42 @@ import { StatisticItems } from 'components/StatistycItems/StatisticItems';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { SectionTitle } from './SectionTitle/SectionTitle';
 import { NotificationMsg } from './NotificationMsg/NotificationMsg';
+
 class App extends Component {
   static defaultProps = {
     initialGood: 0,
     initialNeutral: 0,
     initialBad: 0,
   };
+
   state = {
     good: this.props.initialGood,
     neutral: this.props.initialNeutral,
     bad: this.props.initialBad,
   };
+
   StatisticGoodCounter = () => {
     this.setState(prevState => ({
       good: (prevState.good += 1),
     }));
   };
+
   StatisticNeutralCounter = () => {
     this.setState(prevState => ({
       neutral: (prevState.neutral += 1),
     }));
   };
+
   StatisticBadCounter = () => {
     this.setState(prevState => ({
       bad: (prevState.bad += 1),
     }));
   };
+
   countTotalFeedback = () => {
     return this.state.good + this.state.neutral + this.state.bad;
   };
+
   countPositiveFeedbackPercentage = () => {
     const total = this.state.good + this.state.neutral + this.state.bad;
     const positive = this.state.good * 100;
@@ -40,6 +47,7 @@ class App extends Component {
     }
     return Math.round(positive / total);
   };
+  
   render() {
     const total = this.state.good + this.state.neutral + this.state.bad;
 
--- src/components/StatistycItems/StatisticItems.styled.js
+++ src/components/StatistycItems/StatisticItems.styled.js
@@ -1,12 +1,12 @@
-import styled from '@emotion/styled';
+import styled from 'styled-components';
 export const Box = styled.div`
-  border-top: 1px solid #d4d5ea;
-  border-left: 1px solid #d4d5ea;
-  border-right: 1px solid white;
-  border-bottom: 1px solid white;
+  border-top: 1px solid #151515;
+  border-left: 1px solid #151515;
+  border-right: 1px solid #151515;
+  border-bottom: 1px solid #151515;
+  border-radius: 4px;
   padding: 30px;
   width: 400px;
-  text-transform: uppercase;
   font-size: 30px;
   font-weight: bold;
 `;
@@ -14,10 +14,11 @@ export const Item = styled.p`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-top: 1px solid #d4d5ea;
-  border-right: 1px solid #d4d5ea;
-  border-bottom: 1px solid white;
-  border-left: 1px solid white;
+  border-top: 1px solid #151515;
+  border-right: 1px solid #151515;
+  border-bottom: 1px solid #f8f8f8;
+  border-left: 1px solid #f8f8f8;
+  border-radius: 4px;
   padding: 10px;
-  color: #6b68b9;
+  color: #2b2b2b;
 `;
